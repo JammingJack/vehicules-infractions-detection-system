8521cdf830d5db99174c77cfb4d694362cae1abc
JammingJack <amineseth1@gmail.com>
2022-01-14 09:59:31 +0100

    initial commit\n\n root folder dependencies

---
+++ src/main/java/org/enset/vehiculesinfractionsdetectionsystem/VehiculesInfractionsDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package org.enset.vehiculesinfractionsdetectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculesInfractionsDetectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehiculesInfractionsDetectionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/enset/vehiculesinfractionsdetectionsystem/VehiculesInfractionsDetectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enset.vehiculesinfractionsdetectionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculesInfractionsDetectionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
